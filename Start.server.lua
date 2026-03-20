05a197523a97cfd6c249a846514acb7930a0079c
yoji <125943287+YojiTheOG@users.noreply.github.com>
2026-03-19 21:12:59 -0600

    Start.server.lua

---
+++ Start.server.lua
@@ -0,0 +1,7 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+
+local InventoryService = require("./Services/InventoryService/InventoryServiceServer")
+
+
+InventoryService:init()
